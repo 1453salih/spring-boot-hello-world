20cb2502f480661be95b0e4951e69af8c44f8c33
Salih Korkmaz <salihkorkmaz108478@gmail.com>
2024-07-20 17:12:44 +0300

    first commit

--- src/main/java/ders_video/helloworld/HelloWorldApplication.java
+++ src/main/java/ders_video/helloworld/HelloWorldApplication.java
@@ -2,6 +2,8 @@ package ders_video.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
 public class HelloWorldApplication {
--- src/main/java/ders_video/helloworld/controller/HelloController.java
+++ src/main/java/ders_video/helloworld/controller/HelloController.java
@@ -1,8 +1,14 @@
 package ders_video.helloworld.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+//GET   :Veri görüntülemek istediğimiz zaman kullanılır.   @GetMapping
+//POST  :Veri kaydetmek istediğimiz zaman kullanılır.      @PostMapping
+//PUT   :Veri güncellemek için kullanılır.                 @PutMapping
+//PATCH :Verinin bir bölümünü güncellemek için kullanılır. @PatchMapping
+//       Örn:Parola güncellemek gibi.
+//DELETE:Veri silmek istediğimiz zaman kullanırız.         @DeleteMapping
+
 
 @RestController
 @RequestMapping(path="/api")
@@ -13,6 +19,14 @@ public class HelloController {
     @GetMapping("/hello")
     public  String sayHello(){
         return "hello world!";
+    }
+    @PostMapping("/save")
+    public String save(){
+        return "Data saved!";
+    }
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "Data deleted!";
     }
 
 }
